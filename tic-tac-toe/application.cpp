e0779eb9de1a1a40ab3425b571587ca305d360ee
dingjun168 <dingjun168@gmail.com>
2017-03-22 17:08:56 -0700

    add textures

--- tic-tac-toe/application.cpp
+++ tic-tac-toe/application.cpp
@@ -1,51 +1,6 @@
 #include "application.h"
 
-/*
-* Loads an image into a texture on the rendering device
-* @param file The image file to load
-* @param ren The renderer to load the texture onto
-* @return the loaded texture, or nullptr if something went wrong.
-*
-SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
-  //SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
-  if (texture == nullptr) {
-    //logSDLError(std::cout, "LoadTexture");
-  }
-  return texture;
-}
-/*
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-* width and height
-* @param tex The source texture we want to draw
-* @param rend The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param w The width of the texture to draw
-* @param h The height of the texture to draw
-*
-void renderTexture(
-  SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h) {
-  // Setup the destination rectangle to be at the position we want
-  SDL_Rect dst;
-  dst.x = x;
-  dst.y = y;
-  dst.w = w;
-  dst.h = h;
-  SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-/*
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height
-* @param tex The source texture we want to draw
-* @param rend The renderer we want to draw too
-* @param x The x coordinate to draw too
-* @param y The y coordinate to draw too
-*
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
-  int w, h;
-  SDL_QueryTexture(tex, NULL, NULL, &w, &h);
-  renderTexture(tex, ren, x, y, w, h);
-}*/
+const std::string Application::kResPath = "../res/";
 
 int Application::Execute() {
   if (Initialize() == false) {
@@ -54,23 +9,17 @@ int Application::Execute() {
 
   SDL_Event event;
   bool quit = false;
-
   while (quit == false) {
     // poll event
     while (SDL_PollEvent(&event) != 0) {
       PollEvent(event, quit);
     }
 
-    Update();
+    //Update();
     Render();
   }
 
-  // clean up
-  Cleanup(renderer, window);
-  //IMG_Quit();
-  //TTF_Quit();
-  SDL_Quit();
-
+  Cleanup();
   return 0;
 }
 
@@ -78,6 +27,13 @@ bool Application::Initialize() {
   // start up SDL and make sure it went ok
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     LogSDLError(std::cout, "SDL_Init");
+    return false;
+  }
+
+  // initialize SDL_image
+  if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+    LogSDLError(std::cout, "IMG_Init");
+    Cleanup();
     return false;
   }
 
@@ -92,7 +48,7 @@ bool Application::Initialize() {
     kWindowWidth, kWindowHeight, SDL_WINDOW_SHOWN);
   if (window == nullptr) {
     LogSDLError(std::cout, "SDL_CreateWindow");
-    SDL_Quit();
+    Cleanup();
     return false;
   }
 
@@ -101,16 +57,34 @@ bool Application::Initialize() {
     window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (renderer == nullptr) {
     LogSDLError(std::cout, "SDL_CreateRenderer");
-    Cleanup(window);
-    SDL_Quit();
+    Cleanup();
     return false;
   }
 
+  // load textures
+  std::string board_file = kResPath + "board.png";
+  std::string x_file = kResPath + "x.png";
+  std::string o_file = kResPath + "o.png";
+  textures.push_back(IMG_LoadTexture(renderer, board_file.c_str()));
+  textures.push_back(IMG_LoadTexture(renderer, x_file.c_str()));
+  textures.push_back(IMG_LoadTexture(renderer, o_file.c_str()));
+  for (auto texture : textures) {
+    if (texture == nullptr) {
+      Cleanup();
+      return false;
+    }
+  }
   return true;
 }
 
 void Application::PollEvent(const SDL_Event& event, bool& quit) {
   switch (event.type) {
+  case SDL_MOUSEBUTTONDOWN:
+    // get mouse position
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+
+    break;
   case SDL_KEYDOWN:
     if (event.key.keysym.sym == SDLK_ESCAPE) {
       quit = true;
@@ -158,42 +132,50 @@ void Application::Update() {
 
 void Application::Render() {
   SDL_RenderClear(renderer);
+  RenderTexture(textures[0], 0, 0);
+  RenderTexture(textures[1], 0, 0);
+  RenderTexture(textures[1], 130, 0);
+  RenderTexture(textures[1], 260, 0);
+  RenderTexture(textures[1], 0, 130);
+  RenderTexture(textures[1], 130, 130);
+  RenderTexture(textures[1], 260, 130);
+  RenderTexture(textures[1], 0, 260);
+  RenderTexture(textures[1], 130, 260);
+  RenderTexture(textures[1], 260, 260);
   SDL_RenderPresent(renderer);
+}
+
+void Application::RenderTexture(SDL_Texture* texture,
+  const int& x, const int& y, int w, int h) {
+  // Setup the destination rectangle to be at the position we want
+  SDL_Rect rect;
+  if (w < 0 || h < 0) {
+    SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+  }
+  rect.x = x;
+  rect.y = y;
+  rect.w = w;
+  rect.h = h;
+  SDL_RenderCopy(renderer, texture, NULL, &rect);
+}
+
+void Application::Cleanup() {
+  for (auto texture : textures) {
+    if (texture) {
+      SDL_DestroyTexture(texture);
+    }
+  }
+  if (renderer) {
+    SDL_DestroyRenderer(renderer);
+  }
+  if (window) {
+    SDL_DestroyWindow(window);
+  }
+
+  IMG_Quit();
+  SDL_Quit();
 }
 
 void Application::LogSDLError(std::ostream& os, const std::string& msg) const {
   os << msg << " error: " << SDL_GetError() << std::endl;
-}
-
-template<typename T, typename... Args>
-inline void Application::Cleanup(T* t, Args&&... args) {
-  // clean up the first item in the list
-  Cleanup(t);
-  // recurse to clean up the remaining arguments
-  Cleanup(std::forward<Args>(args)...);
-}
-
-template<>
-inline void Application::Cleanup<SDL_Window>(SDL_Window* window) {
-  if (window) {
-    SDL_DestroyWindow(window);
-  }
-}
-template<>
-inline void Application::Cleanup<SDL_Renderer>(SDL_Renderer* renderer) {
-  if (renderer) {
-    SDL_DestroyRenderer(renderer);
-  }
-}
-template<>
-inline void Application::Cleanup<SDL_Texture>(SDL_Texture* texture) {
-  if (texture) {
-    SDL_DestroyTexture(texture);
-  }
-}
-template<>
-inline void Application::Cleanup<SDL_Surface>(SDL_Surface* surface) {
-  if (surface) {
-    SDL_FreeSurface(surface);
-  }
 }
--- tic-tac-toe/application.h
+++ tic-tac-toe/application.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "state.h"
@@ -15,44 +16,44 @@ public:
   int Execute();
 
 private:
-  static const int kWindowWidth = 640;
-  static const int kWindowHeight = 480;
+  static const int kWindowWidth = 390;
+  static const int kWindowHeight = 390;
+  static const std::string kResPath;
 
   bool Initialize();                              // initialize SDL application
   void PollEvent(const SDL_Event& event, bool& quit);     // capture SDL Events
   void Update();
   void Render();
 
+  /*
+  * Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+  * width and height
+  * @param tex The source texture we want to draw
+  * @param rend The renderer we want to draw too
+  * @param x The x coordinate to draw too
+  * @param y The y coordinate to draw too
+  * @param w The width of the texture to draw
+  * @param h The height of the texture to draw
+  */
+  void RenderTexture(SDL_Texture* texture,
+    const int& x, const int& y, int w = -1, int h = -1);
+
+  void Cleanup();
+
+  /*
+  * Log an SDL error with some error message to the output stream of our choice
+  * @param os The output stream to write the message to
+  * @param msg The error message to write, format will be msg error: SDL_GetError()
+  */
   void LogSDLError(std::ostream& os, const std::string& msg) const;
-
-  /*
-  * Recurse through the list of arguments to clean up, cleaning up
-  * the first one in the list each iteration.
-  */
-  template<typename T, typename... Args>
-  void Cleanup(T* t, Args&&... args);
-
-  /*
-  * These specializations serve to free the passed argument and also provide the
-  * base cases for the recursive call above, eg. when args is only a single item
-  * one of the specializations below will be called by
-  * cleanup(std::forward<Args>(args)...), ending the recursion.
-  */
-  template<>
-  inline void Cleanup<SDL_Window>(SDL_Window *window);
-  template<>
-  inline void Cleanup<SDL_Renderer>(SDL_Renderer *renderer);
-  template<>
-  inline void Cleanup<SDL_Texture>(SDL_Texture *texture);
-  template<>
-  inline void Cleanup<SDL_Surface>(SDL_Surface *surface);
 
   State state;
   AI ai;
-  
+
   std::string title;
   SDL_Window* window;
   SDL_Renderer* renderer;
+  std::vector<SDL_Texture*> textures;
 };
 
 #endif  // APPLICATION_H_
